87b4c2bc1c32b4e0bad61e936235b59c4ef7e389
jatinkala2004 <kalajatin94@gmail.com>
2026-06-25 01:24:40 +0530

    port changes

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 const fs = require("fs");
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors());
@@ -73,5 +73,5 @@ app.delete("/api/notes/:id", (req, res) => {
 
 // Start Server
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
